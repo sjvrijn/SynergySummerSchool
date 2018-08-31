c0ffc1c101262273150b8bbffde8f4ba64ee9e0f
Sander <svr003@gmail.com>
2018-08-31 09:34:48 +0200

    Refactoring: separated out several smaller functions

--- translator.py
+++ translator.py
@@ -40,15 +40,23 @@ def mem_scan(matrix):
     return list(reversed(memories))
 
 
-def translation(n_inputs, matrix):
+def evalute_circuit_representation(n_inputs, matrix, input_bits):
 
     global_inputs = [pyrtl.Input(1, str(inp)) for inp in range(n_inputs)]
     inputs = mem_scan(matrix)
     num_memories = len(inputs)
     cur_memory = num_memories - 1
     inputs.extend(global_inputs)
-    gate_matrix = []
 
+    gate_matrix = translate_matrix_to_pyrtl(cur_memory, inputs, matrix, num_memories)
+    outputs = connect_outputs(gate_matrix, matrix)
+    output_bits = simulate_circuit(input_bits, inputs, outputs, n_inputs, num_memories)
+
+    # return fitness(output_bits)
+
+
+def translate_matrix_to_pyrtl(cur_memory, inputs, matrix, num_memories):
+    gate_matrix = []
     for column in matrix:
         gate_column = []
         gate_matrix.append(gate_column)
@@ -58,27 +66,34 @@ def translation(n_inputs, matrix):
                 cur_memory -= 1
             gate_column.append(gate_func)
         inputs.extend(gate_column)
+    return gate_matrix
 
+
+def connect_outputs(gate_matrix, matrix):
     outputs = [pyrtl.Output(1, f'out_{i}') for i in range(len(matrix[-1]))]
     for out, gate in zip(outputs, gate_matrix[-1]):
         out <<= gate
+    return outputs
 
+
+def simulate_circuit(input_bits, inputs, outputs, n_inputs, num_memories):
     sim_trace = pyrtl.SimulationTrace()
     sim = pyrtl.Simulation(tracer=sim_trace)
-
-    input_bits = ['000',
-                  '001',
-                  '000',
-                  '111',
-                  '000',
-                  '001']
-
+    output = []
     for bits in input_bits:
-
-        bits_in = {inp.name: int(bit) for inp, bit in zip(inputs[num_memories:num_memories+n_inputs], bits)}
+        bits_in = {inp.name: int(bit) for inp, bit in zip(inputs[num_memories:num_memories + n_inputs], bits)}
         sim.step(bits_in)
-        print(f'{bits_in} --> {[sim.inspect(out) for out in outputs]}')
+        output.append([sim.inspect(out) for out in outputs])
+    return output
 
+
+input_bits = ['000',
+              '001',
+              '000',
+              '111',
+              '000',
+              '000',
+              '001']
 
 matrix = [
     [
@@ -91,4 +106,4 @@ matrix = [
     ],
 ]
 
-translation(3,matrix)
+evalute_circuit_representation(3, matrix, input_bits)
